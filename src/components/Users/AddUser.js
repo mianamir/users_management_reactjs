a2593bded1cc6c2e47155994d3ab55d9a3214e58
Muhammad Amir <63117404+amirsavvy4ever@users.noreply.github.com>
2021-12-29 15:28:50 +0500

    managing the add user components states
    managing the add user components states and code refactoring!

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button';
 
 import Card from '../UI/Card';
 import classes from '../Users/AddUser.module.css';
 
 
-const AddUser = props => {
+const AddUser = (props) => {
+
+    const [enteredUserName, setEnteredUserName] = useState('');
+
+    const [enteredAge, setEnteredAge] = useState('');
+
+
 
     const addUserHandler = (event) => {
         event.preventDefault();
 
+        console.log(enteredUserName, enteredAge);
 
+    };
+
+    const userNameChangeHandler = (event) => {
+        setEnteredUserName(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     };
 
     return (
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor='username'>Username</label>
-            <input id='username' type="text"/>
+            <input id='username' type="text" onChange={ userNameChangeHandler }/>
             <label htmlFor='age'>Age (Years)</label>
-            <input id='age' type="number"/>
+            <input id='age' type="number" onChange={ ageChangeHandler }/>
 
             <Button type='submit'>Add User</Button>
         </form>
