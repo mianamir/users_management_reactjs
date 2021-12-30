88be8abeee5de23c9bd3882ac90f8c5836f3b346
Muhammad Amir <63117404+amirsavvy4ever@users.noreply.github.com>
2021-12-30 21:34:17 +0500

    users list with uplift state
    users list with uplift state in the components!

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -16,7 +16,20 @@ const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefault();
 
-        console.log(enteredUserName, enteredAge);
+        if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
+            return;
+        }
+
+        if (+enteredAge < 1) {
+            return;
+        }
+
+        props.onAddUser(enteredUserName, enteredAge);
+
+        // reset the input form data
+        setEnteredUserName('');
+        setEnteredAge('');
+
 
     };
 
@@ -32,9 +45,9 @@ const AddUser = (props) => {
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor='username'>Username</label>
-            <input id='username' type="text" onChange={ userNameChangeHandler }/>
+            <input id='username' type="text" value={enteredUserName} onChange={ userNameChangeHandler }/>
             <label htmlFor='age'>Age (Years)</label>
-            <input id='age' type="number" onChange={ ageChangeHandler }/>
+            <input id='age' type="number" value={enteredAge} onChange={ ageChangeHandler }/>
 
             <Button type='submit'>Add User</Button>
         </form>
